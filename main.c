13328c559f5802ed2745bf90453f103618706647
Ryan Chao <ryanchao2012@gmail.com>
2016-08-12 07:19:06 +0000

    initial commit

--- main.c
+++ main.c
@@ -33,10 +33,12 @@
 #define GPIO_PIN2                                12
 #define DMA                                      5
 
-// #define STRIP_TYPE				             WS2811_STRIP_RGB		// WS2812/SK6812RGB integrated chip+leds
+// #define STRIP_TYPE				                 WS2811_STRIP_RGB		// WS2812/SK6812RGB integrated chip+leds
 // #define STRIP_TYPE				             WS2811_STRIP_GBR		// WS2812/SK6812RGB integrated chip+leds
 // #define STRIP_TYPE				             SK6812_STRIP_RGBW		// SK6812RGBW (NOT SK6812RGB)
-#define STRIP_TYPE                               WS2811_STRIP_BRG
+
+#define STRIP_TYPE                               WS2811_STRIP_GRB
+// #define STRIP_TYPE                               WS2811_STRIP_BRG
 
 	
 #define UDP_PORT 								 6454
